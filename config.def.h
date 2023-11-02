a22244b184ca1a17576a944a8e4db194bc0e45f5
Alexander Balgavy <alexander.balgavy@spaceapplications.com>
2023-11-02 16:59:36 +0100

    control-clear patch
    https://tools.suckless.org/slock/patches/control-clear/slock-git-20161012-control-clear.diff

--- config.def.h
+++ config.def.h
@@ -11,3 +11,6 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* allow control key to trigger fail on clear */
+static const int controlkeyclear = 0;
